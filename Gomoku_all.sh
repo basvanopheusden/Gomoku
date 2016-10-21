39e9b246f79470870fe8868a0767823f62bd0907
basvanopheusden <svo213@nyu.edu>
2016-10-20 20:45:40 -0400

    added job script

--- Gomoku_all.sh
+++ Gomoku_all.sh
@@ -3,14 +3,14 @@
 #PBS -l nodes=1:ppn=20
 #PBS -l walltime=24:00:00
 #PBS -l mem=6GB
-#PBS -t 0-33349
+#PBS -t 0-31899
 #PBS -j oe
 #PBS -M svo213@nyu.edu
-#PBS -m abe
+#PBS -m abe 
 
 i=${PBS_ARRAYID}
-allmodels=(final final_noprune final_nodelta final_noact final_notree final_nocenter final_no2conn final_no2unc final_no3 final_no4 final_nonoise final_diag final_vert final_triangle final_drop_hvd final_drop_type final_opp final_fixed_iters final_fixed_branch final_fixed_depth mcts final_drop_tile final_opt_weights)
-allmodels2=(final final final final final final final final final final final final final final final final final final_fixed_iters final_fixed_branch final_fixed_depth mcts final_drop_tile final)
+allmodels=(final final_noprune final_nodelta final_noact final_notree final_nocenter final_no2conn final_no2unc final_no3 final_no4 final_nonoise final_weight_hvd final_triangle final_drop_hvd final_drop_type final_opp final_fixed_iters final_fixed_branch final_fixed_depth final_mcts_myopic final_mcts_rand final_drop_tile final_opt_weights)
+allmodels2=(final final final final final final final final final final final final final final final final final_fixed_iters final_fixed_branch final_fixed_depth mcts mcts final_drop_tile final)
 alldatas=(hvh gen eye learn1 learn2 learn3 tai1 tai2 tur)
 allNs=(40 40 10 50 50 50 50 40 30)
 
